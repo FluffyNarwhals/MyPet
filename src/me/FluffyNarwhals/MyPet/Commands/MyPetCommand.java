d05103f5c527072e84f6503652c2063a1c4a6f57
FluffyNarwhals <FluffyNarwhalsGaming@gmail.com>
2013-08-04 15:35:15 -0700

    Basic Setup for Plugin.

---
+++ src/me/FluffyNarwhals/MyPet/Commands/MyPetCommand.java
@@ -0,0 +1,53 @@
+package me.FluffyNarwhals.MyPet.Commands;
+
+import me.FluffyNarwhals.MyPet.Plugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MyPetCommand implements CommandExecutor{
+	
+	private static Plugin pl = Plugin.pl;
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		if(sender instanceof Player){
+			
+			Player p = (Player) sender;
+			
+			if(args.length == 0){
+				
+				p.sendMessage(pl.getPrefix() + ChatColor.DARK_GREEN + "Name: " + ChatColor.DARK_AQUA + "MyPet");
+				p.sendMessage(pl.getPrefix() + ChatColor.DARK_GREEN + "Version: " + ChatColor.DARK_AQUA + Plugin.p.getVersion());
+				p.sendMessage(pl.getPrefix() + ChatColor.DARK_GREEN + "Creator: " + ChatColor.DARK_AQUA + "FluffyNarwhals");
+				p.sendMessage(pl.getPrefix() + "Type /mypet help");
+				
+			}
+			
+			if(args.length > 0){
+				
+				String command = args[0];
+				
+				if(command.equalsIgnoreCase("help")){
+					
+					p.sendMessage(pl.getPrefix() + "/mypet help - Gives list of commands");
+					p.sendMessage(pl.getPrefix() + "/mypet name - Renames your pet");
+					p.sendMessage(pl.getPrefix() + "/mypet tpto - Teleports to your pet");
+					p.sendMessage(pl.getPrefix() + "/mypet tphere - Teleports your pet to you");
+					p.sendMessage(pl.getPrefix() + "/mypet mode <passive/aggressive> - Sets your pet's mode");
+					p.sendMessage(pl.getPrefix() + "/mypet buy - Buys you a pet");
+					
+				}
+				
+			}
+			
+		}
+		
+		return false;
+		
+	}
+	
+}
---
+++ src/me/FluffyNarwhals/MyPet/Plugin.java
@@ -0,0 +1,33 @@
+package me.FluffyNarwhals.MyPet;
+
+import me.FluffyNarwhals.MyPet.Commands.*;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin{
+	
+	public static Plugin pl;
+	public static PluginDescriptionFile p;
+	
+	public void onEnable(){
+		
+		pl = this;
+		p = this.getDescription();
+		
+		this.getCommand("mypet").setExecutor(new MyPetCommand());
+		
+		this.config();
+		
+	}
+	
+	public void config(){
+		this.saveDefaultConfig();
+	}
+	
+	public String getPrefix(){
+		return ChatColor.GREEN + "[" + ChatColor.AQUA + "MyPet" + ChatColor.GREEN + "] " + ChatColor.DARK_AQUA;
+	}
+	
+}
